fedc8612446088074997f022056c4f75286baa0e
binZuy <duynguyenminh0206@gmail.com>
2024-06-10 11:31:08 +0700

    Update 2

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include('./helper/FunctionController.php');
 
 <head>
     <meta charset="utf-8" />
-    <title>CLASSROOM INVENTORY MANAGEMENT SYSTEM</title>
+    <title>CLASSROOM MANAGEMENT SYSTEM</title>
     <!-- Favicon-->
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="css/styles.css" rel="stylesheet" />
@@ -17,7 +17,7 @@ include('./helper/FunctionController.php');
     <div class="d-flex" id="wrapper">
         <!-- Sidebar-->
         <div class="border-end bg-white" id="sidebar-wrapper">
-            <div class="sidebar-heading border-bottom bg-light" style="font-size:30px;">CIMS</div>
+            <div class="sidebar-heading border-bottom bg-light" style="font-size:30px;">CMS</div>
             <div class="list-group list-group-flush">
                 <a class="list-group-item list-group-item-action list-group-item-light p-3" href="index.php">Home</a>
                 <a class="list-group-item list-group-item-action list-group-item-light p-3" href="admin/">Admin Login</a>
@@ -30,11 +30,11 @@ include('./helper/FunctionController.php');
             <!-- Top navigation-->
             <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
                 <div class="container-fluid">
-                    <button class="btn btn-primary" id="sidebarToggle">Toggle Menu</button>
+                    <button class="btn btn-primary" id="sidebarToggle">Dashboard</button>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                            <li class="nav-item active" style="font-size:30px;">CLASSROOM INVENTORY MANAGEMENT SYSTEM</li>
+                            <li class="nav-item active" style="font-size:30px;">CLASSROOM MANAGEMENT SYSTEM</li>
                         </ul>
                     </div>
                 </div>
